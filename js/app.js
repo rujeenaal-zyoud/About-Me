0cf64e31b19db19fcc66ece4b566f1e0ec7250ea
rujeenaal-zyoud <rujeenaalzoud@gmail.com>
2021-03-29 22:34:21 +0300

    codeing

--- js/app.js
+++ js/app.js
@@ -3,9 +3,9 @@
 var NAME = prompt("Please enter your name");
 function hi(yourname) {
     if (NAME === null && NAME === " ") {
-        var hello = "hi ,welcome in My sweet webpage ";
+        var hello = "hi ,welcome in About me webpage ";
     }
-    else { var hello = "hi " + NAME + ",welcome in  My sweet webpage"; }
+    else { var hello = "hi " + NAME + ",welcome in  My About me webpage"; }
 
     alert(hello);
 }
@@ -27,18 +27,18 @@ let age = prompt('What my age?');
 age = Number(age);
 //console.log(age);
 if (age === '22') {
-//console.log('You know my age Iam happy');
+    //console.log('You know my age Iam happy');
     alert('You know my age Iam happy');
 }
 if (age >= 23 || age < 22) {
-//console.log(alert('You dont know me ,but you can try :( '));
+    //console.log(alert('You dont know me ,but you can try :( '));
     alert('You dont know me ,but you can try :( ');
 }
 
 
 let movie = prompt('What is my favourite movie siccin/zompi?');
 movie = movie.toLowerCase();
-console.log(movie);
+//console.log(movie);
 if (movie == 'siccin') {
     //console.log('wow that right');
     alert('wow that right');
@@ -57,10 +57,10 @@ else {
 
 let favcolour = prompt('what my lovely color the pink/gray ? ')
 
-console.log(favcolour);
+//console.log(favcolour);
 switch (favcolour.toUpperCase()) {
     case 'PINK':
-       // console.log('Yes this my lovely color');
+        // console.log('Yes this my lovely color');
         alert('Yes this my lovely color');
         break;
     case 'GRAY':
@@ -73,20 +73,20 @@ switch (favcolour.toUpperCase()) {
 
 let meal = prompt('What my fav meal rice/meat?')
 meal = meal.toUpperCase();
-console.log(meal);
+//console.log(meal);
 if (meal == 'RICE') {
     //console.log('Yes I love rice ')
     alert('Yes I love rice ')
 }
 if (meal == 'meat') {
-   // console.log('NO I am vegetarian');
+    // console.log('NO I am vegetarian');
     alert('NO I am vegetarian');
 }
 
 
 let fruit = prompt("The last question :), what my fav fruit apple / bannana?");
 fruit = fruit.toLowerCase();
-console.log(fruit);
+//console.log(fruit);
 if (fruit == 'bannana') {
     //console.log("I dont love bannana may you love it");
     alert("I dont love bannana may you love it");
